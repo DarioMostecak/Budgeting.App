807899baff91b7b8174b1532051198cf353d56c5
DarioMostecak <dariomostecak@gmail.com>
2023-03-21 21:00:30 +0100

    Add unit test RetrieveUserByIdAsync - Logic - Passed.

---
+++ Budgeting.App.Api.Tests.Unit/Services/Foundations/Users/UserServiceTests.Logic.RetrieveUserById.cs
@@ -0,0 +1,41 @@
+﻿using Budgeting.App.Api.Models.Users;
+using FluentAssertions;
+using Force.DeepCloner;
+using Moq;
+using System;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Budgeting.App.Api.Tests.Unit.Services.Foundations.Users
+{
+    public partial class UserServiceTests
+    {
+        [Fact]
+        public async Task ShouldRetrieveApplicationUserById()
+        {
+            //given
+            User randomUser = CreateUser();
+            User storageUser = randomUser;
+            User expectedUser = storageUser.DeepClone();
+            Guid userId = randomUser.Id;
+
+            this.userManagerBrokerMock.Setup(manager =>
+                manager.SelectUserByIdAsync(userId))
+                  .ReturnsAsync(storageUser);
+
+            //when
+            User actualUser =
+                await this.userService.RetrieveUserByIdAsync(userId);
+
+            //then
+            actualUser.Should().BeEquivalentTo(expectedUser);
+
+            this.userManagerBrokerMock.Verify(manager =>
+                manager.SelectUserByIdAsync(It.IsAny<Guid>()),
+                  Times.Once);
+
+            this.userManagerBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
