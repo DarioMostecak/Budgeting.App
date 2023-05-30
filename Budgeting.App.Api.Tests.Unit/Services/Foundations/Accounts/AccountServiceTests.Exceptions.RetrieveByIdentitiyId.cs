eb242c0c801addcde4286b08bcdc865891db0255
DarioMostecak <dariomostecak@gmail.com>
2023-05-30 19:54:32 +0200

    add => unit test foundation accountService => RetrieveAccoutByUserIdentityId => AccountServiceException => PASS.

--- Budgeting.App.Api.Tests.Unit/Services/Foundations/Accounts/AccountServiceTests.Exceptions.RetrieveByIdentitiyId.cs
+++ Budgeting.App.Api.Tests.Unit/Services/Foundations/Accounts/AccountServiceTests.Exceptions.RetrieveByIdentitiyId.cs
@@ -8,6 +8,7 @@ using Budgeting.App.Api.Models.Accounts;
 using Budgeting.App.Api.Models.Accounts.Exceptions;
 using MongoDB.Driver;
 using Moq;
+using System;
 using System.Threading.Tasks;
 using Xunit;
 
@@ -43,6 +44,43 @@ namespace Budgeting.App.Api.Tests.Unit.Services.Foundations.Accounts
             this.loggingBrokerMock.Verify(broker =>
                  broker.LogError(It.Is(SameExceptionAs(
                      expectedAccountDependencyException))),
+                      Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                 broker.SelectAccountByUserIdentityIdAsync(It.IsAny<string>()),
+                  Times.Once);
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+        }
+
+        [Fact]
+        public async Task ShouldThrowServiceExceptionOnRetrieveByIdentityIdIfExceptionOccurresAndLogItAsync()
+        {
+            //given
+            string randomUserIdentityId = GetRandomIdentityUserId();
+            var serviceException = new Exception();
+
+            var failedAccountServiceException =
+                new FailedAccountServiceException(serviceException);
+
+            var expectedAccountServiceException =
+                new AccountServiceException(serviceException);
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.SelectAccountByUserIdentityIdAsync(It.IsAny<string>()))
+                       .ThrowsAsync(serviceException);
+
+            //when
+            ValueTask<Account> retrieveAccountByIdentityIdTask =
+                this.accountService.RetrieveAccountByUserIdentityIdAsync(randomUserIdentityId);
+
+            await Assert.ThrowsAsync<AccountServiceException>(() =>
+                 retrieveAccountByIdentityIdTask.AsTask());
+
+            this.loggingBrokerMock.Verify(broker =>
+                 broker.LogError(It.Is(SameExceptionAs(
+                     expectedAccountServiceException))),
                       Times.Once);
 
             this.storageBrokerMock.Verify(broker =>
